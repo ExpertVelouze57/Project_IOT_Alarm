d7b6d6514d84527484502d7cc3e07c3efae9241f
Mathis <vernay.mathis@gmail.com>
2022-01-13 23:32:30 +0100

    initial commit (helloworld)

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+#include "net/loramac.h"
+#include "semtech_loramac.h"
+
+
+#include "board.h"
+
+/* TODO: Add the cayenne_lpp header here */
+#include "cayenne_lpp.h"
+
+int main(void){
+    printf("helloWorld!!!\n");
+    return 0;
+}
